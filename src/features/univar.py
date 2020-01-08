c26a7d9821136c44c7df307264785da313954377
Thibault Grandjean <thibault.grandjean@protonmail.ch>
2020-01-08 15:55:11 +0100

    progress in univariate analysis.

--- src/features/univar.py
+++ src/features/univar.py
@@ -7,6 +7,7 @@ Thibault Grandjean
 
 import warnings
 
+from IPython.display import display, HTML
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -41,8 +42,14 @@ class UnivariateAnalysis(object):
     def series_stats(self, column):
         """Return stats for a Series."""
         series = self.data[column]
-        return [['mean', 'std', 'min', 'max'],
-                [series.mean(), series.std(), series.min(), series.max()]]
+        return [['mean', 'std',
+                 'min', 'max',
+                 'median', 'variance',
+                 '25%', '75%'],
+                [series.mean(), series.std(),
+                 series.min(), series.max(),
+                 series.median(), series.var(),
+                 series.quantile(0.25), series.quantile(0.75)]]
 
     def graph_series(self, column):
         """Make a plot for a series."""
@@ -66,5 +73,37 @@ class UnivariateAnalysis(object):
     def make_analysis(self, column):
         """Make full analysis."""
         print(self.completion_rate(column))
-        print(tabulate(self.series_stats(column), tablefmt="grid"))
+        tab = tabulate(self.series_stats(column), tablefmt="html")
+        display(HTML(tab))
+        upper, lower = self.outliers_infos(column)
+        display(lower)
+        display(upper)
         self.graph_series(column)
+
+    def outliers_infos(self, column):
+        """Return informations for values out of range of 1st quantile
+        and the 3rd."""
+        lower = self.data[self.data[column] <\
+         self.data[column].quantile(0.25)].copy()
+        upper = self.data[self.data[column] >\
+         self.data[column].quantile(0.75)].copy()
+        lower_grp = lower.groupby('main_category_en').count()
+        upper_grp = upper.groupby('main_category_en').count()
+        sort_kwgrs = dict(by=f'{column}_mean', ascending=False)
+        cols = ['product_name', column]
+        means_low = lower.groupby('main_category_en')[column].mean()
+        means_upp = upper.groupby('main_category_en')[column].mean()
+        means_low.dropna(inplace=True)
+        means_upp.dropna(inplace=True)
+        means_low.name = column + '_mean'
+        means_upp.name = column + '_mean'
+        means_low = means_low.reset_index()
+        means_upp = means_upp.reset_index()
+        lower_grp = lower_grp[lower_grp['product_name'] != 0]
+        upper_grp = upper_grp[upper_grp['product_name'] != 0]
+        lower_grp = lower_grp[cols]
+        upper_grp = upper_grp[cols]
+        lower_grp.reset_index(inplace=True, drop=False)
+        upper_grp.reset_index(inplace=True, drop=False)
+        return (pd.merge(lower_grp, means_low).sort_values(**sort_kwgrs),
+                pd.merge(upper_grp, means_upp).sort_values(**sort_kwgrs))
