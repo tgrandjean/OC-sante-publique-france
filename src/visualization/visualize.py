b936625b9e825c79ebd3b4b038ef777783b7fccb
Thibault Grandjean <thibault.grandjean@protonmail.ch>
2020-01-25 16:12:08 +0100

    :construction_worker: visualization

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -52,13 +52,13 @@ class DistributionPlot(AbstractVisualization):
 
 class RepartitionPlot(AbstractVisualization):
 
-    available_types = ["bar", "pie"]
+    available_types = ["bar", "pie", "stacked-bar"]
 
     def __init__(self, data, var, plot_type, **kwargs):
-        super().__init__(data, **kwargs)
+        super().__init__(data)
         self.var = var
         self.plot_type = plot_type
-        self.max_class = kwargs.get('max_class', 10)
+        self.max_class = kwargs.pop('max_class', 10)
 
     @property
     def plot_type(self):
@@ -71,27 +71,65 @@ class RepartitionPlot(AbstractVisualization):
                              f"in {self.available_types}")
         self._plot_type = plot_type
 
-    def plot(self, **kwargs):
-        if self.plot_type == 'bar':
-            orient = kwargs.get('orient', 'v')
-            data = self.data[self.var].value_counts()
-            data.sort_values()
-            x = data.iloc[:self.max_class].index.values
-            y = data.iloc[:self.max_class].values
-            others = data.iloc[self.max_class:].sum()
+    @property
+    def data_for_plot(self):
+        data = self.data[self.var].value_counts()
+        data.sort_values()
+        x = data.iloc[:self.max_class].index.values
+        y = data.iloc[:self.max_class].values
+        others = data.iloc[self.max_class:].sum()
+        return x, y, others
+
+
+    def pie(self, **kwargs):
+        val = self.data[self.var].value_counts().sort_index()
+        plt.figure(figsize=kwargs.get('figsize', (5, 6)))
+        plt.pie(val,
+                labels=val.index.str.upper(),
+                colors=kwargs.get('colors'),
+                explode=kwargs.get('explode'))
+        plt.axis('equal')
+
+    def bar(self, **kwargs):
+        xlabel, ylabel = kwargs.pop('xlabel', ''), kwargs.pop('ylabel', '')
+        x, y, others = self.data_for_plot
+        if kwargs.pop('others_cat', True):
             x = np.append(x, ['others'])
             y = np.append(y, [others])
-            if orient == 'h':
-                x, y = y, x
-            plt.figure(figsize=kwargs.get('figsize', (12, 8)))
-            sns.barplot(x=x, y=y, **kwargs)
-            plt.xlabel(kwargs.get("xlabel"))
-            plt.ylabel(kwargs.get("ylabel"))
+        if kwargs.get('orient', 'v') == 'h':
+            x, y = y, x
+        xticks_rotation = kwargs.pop('xticks_rotation', 0)
+        plt.figure(figsize=kwargs.pop('figsize', (12, 8)))
+        sns.barplot(x=x, y=y, **kwargs)
+        plt.xticks(rotation=xticks_rotation)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+
+    def stacked_bar(self, hue, **kwargs):
+        data = self.data.copy()
+        count = data[self.var].value_counts().sort_values(ascending=False)
+        top_recurent = count.iloc[:self.max_class].index.values
+        # xlabel, ylabel = kwargs.pop('xlabel', ''), kwargs.pop('ylabel', '')
+        def labelizer(x):
+            return x if x in top_recurent else 'other'
+        data[self.var] = data[self.var].apply(labelizer)
+        data = data.groupby(['brands', 'nutriscore_grade'])\
+        .size().reset_index()\
+        .pivot(columns='brands', index='nutriscore_grade', values=0)
+        if not kwargs.pop('others_cat', True):
+            data.drop('other', axis=1, inplace=True)
+        # plt.figure(figsize=kwargs.pop('figsize', (12, 8)))
+        # data.set_index(hue)\
+        # .reindex(data.set_index(hue).sum().sort_values().index, axis=1)\
+        # .T.plot(kind='bar', stacked=True)
+        return data
+
+    def plot(self, **kwargs):
+        if self.plot_type == 'bar':
+            self.bar(**kwargs)
+
         elif self.plot_type == 'pie':
-            val = self.data[self.var].value_counts().sort_index()
-            plt.figure(figsize=kwargs.get('figsize', (5, 6)))
-            plt.pie(val,
-                    labels=val.index.str.upper(),
-                    colors=kwargs.get('colors'),
-                    explode=kwargs.get('explode'))
-            plt.axis('equal')
+            self.pie(**kwargs)
+
+        elif self.plot_type == 'stacked-bar':
+            return self.stacked_bar(kwargs.pop('hue', None), **kwargs)
